80e14ca92ec70b901801aa2a0736b21e41440137
dev_os <dred.tam@gmail.com>
2021-05-09 17:26:40 +0600

    Yeat not finished angular project

---
+++ src/app/hero.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(
+    private  messageService: MessageService,
+    private  http: HttpClient) { }
+
+  private heroesUrl = 'api/heroes';
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl);
+  }
+
+  getHero(id: number): Observable< Hero > {
+    const hero = HEROES.find(h => h.id === id) as Hero;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+
+  // tslint:disable-next-line:typedef
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+}
